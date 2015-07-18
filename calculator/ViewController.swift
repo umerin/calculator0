12c7d2d1c432010802a26b5d6feed851946c0342
umerin <cvs3dev@gmail.com>
2015-07-18 16:52:57 +0900

    他のボタンもまとめ中

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -16,9 +16,25 @@ class ViewController: UIViewController {
     
     @IBOutlet var label: UILabel!
 
+    @IBOutlet weak var plusB: UIButton!
+    @IBOutlet weak var minusB: UIButton!
+    @IBOutlet weak var kakeruB: UIButton!
+    @IBOutlet weak var waruB: UIButton!
+    @IBOutlet weak var equalB: UIButton!
+    @IBOutlet weak var ACB: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        plusB.tag = 1
+        minusB.tag = 2
+        kakeruB.tag = 3
+        waruB.tag = 4
+        equalB.tag = 5
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,11 +45,46 @@ class ViewController: UIViewController {
     
     
     //数字ボタン
-    @IBAction func SelectNumberButton(sender: AnyObject){
-        number = number * 10 + sender.tag - 100
-        label.text = String(number)
+    @IBAction func selectNumberButton(sender :UIButton){
+        if sender.tag == 1000{
+            number = number * sender.tag / 10
+            label.text = String(number)
+        }else{
+            number = number * 10 + sender.tag - 100
+            label.text = String(number)
+        }
     }
     
+    @IBAction func otherB(sender :UIButton){
+        switch sender.tag{
+        case 1:
+            label.text = "+"
+            operation = 1
+            number1 = number
+        case 2:
+            label.text = "-"
+            operation = 2
+            number1 = number
+        case 3:
+            label.text = "×"
+            operation = 3
+            number1 = number
+        case 4:
+            label.text = "÷"
+            operation = 4
+            number1 = number
+        
+        default:
+            break
+        
+        }
+    }
+    
+    //数字以外のボタン
+    
+    
+    
+    /*
     @IBAction func plus(){
         label.text = "+"
         
@@ -79,7 +130,7 @@ class ViewController: UIViewController {
         operation = 0
         label.text = String(0)
     }
-
+*/
 
 }
 
