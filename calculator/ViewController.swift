f45ed807221a7e0e0bd95314861c6dd2fb960999
umerin <cvs3dev@gmail.com>
2015-07-24 19:22:38 +0900

    シェアボタンの設置

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -63,6 +63,7 @@ class ViewController: UIViewController {
             operation = 1
             number1 = number
             number = 0
+            //plusB.backgroundColor = UIColor(red: 0.7, green: 0.7, blue: 0.7, alpha: 1)
             }
         case 2:if operation == 0{
             label.text = "-"
@@ -102,57 +103,11 @@ class ViewController: UIViewController {
         }
     }
     
-    //数字以外のボタン
-    
-    
-    
-    /*
-    @IBAction func plus(){
-        label.text = "+"
-        
-        operation = 1
-        number1 = number
-        number = 0
+    @IBAction func actionB(sender: UIBarButtonItem) {
+        let controller = UIActivityViewController(activityItems: [label.text!], applicationActivities: nil)
+        self.presentViewController(controller, animated: true, completion: nil)
     }
-    @IBAction func different(){
-        label.text = "-"
-        
-        operation = 2
-        number1 = number
-        number = 0
-    }
-    @IBAction func kakeru(){
-        label.text = "×"
-        
-        operation = 3
-        number1 = number
-        number = 0
-    }
-    @IBAction func waru(){
-        label.text = "÷"
-        
-        operation = 4
-        number1 = number
-        number = 0
-    }
-    @IBAction func equal() {
-        if operation == 1 {
-            label.text = String(number + number1)
-        }else if operation == 2 {
-            label.text = String(number - number1)
-        }else if operation == 3 {
-            label.text = String(number * number1)
-        }else if operation == 4 {
-            label.text = String(number / number1)
-        }
-    }
-    @IBAction func ac() {
-        number = 0
-        number1 = 0
-        operation = 0
-        label.text = String(0)
-    }
-*/
+
 
 }
 
